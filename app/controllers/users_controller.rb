7284c795c73dcf0f055f3658e521e03fad4364d0
Wayne Li <wli2@berkeley.edu>
2017-10-23 08:06:32 +0000

    Start up iter1-2

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,34 @@
+class UsersController < ApplicationController
+  
+  def user_params
+    params.require(:user).permit()
+  end
+
+  def show
+    
+  end
+
+  def index
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+end
