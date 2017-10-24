0e6772695ba045d514d271d258cc2e8af1d85ba5
Wayne Li <wli2@berkeley.edu>
2017-10-24 03:39:37 +0000

    Create db

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,34 +1,34 @@
 class UsersController < ApplicationController
   
   def user_params
-    params.require(:user).permit()
+    params.require(:user).permit(:username, :password, :full_name, :status)
   end
 
   def show
-    
+    # Unused.
   end
 
   def index
-
+    # User dashboard.
   end
 
   def new
-
+    # Form to create. Admin only.
   end
 
   def create
-
+    # Actual post.
   end
 
   def edit
-
+    # Admin only.
   end
 
   def update
-
+    # Do we even use this?
   end
 
   def destroy
-
+    # Delete from DB.
   end
 end
