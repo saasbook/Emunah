7d311d4e32763eb0047a699fc79b8d0ead35d882
Wayne Li <wli2@berkeley.edu>
2017-10-25 04:12:37 +0000

    Refactor home page to be rails convention

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   resources :users
   resources :families
   resources :people
-  resources :home
+  
+  # home doesn't reqiure full resources.
+  get '/home' => 'homes#index', :as => 'home'
+  
   # map '/' to be a redirect to '/movies'
   root :to => redirect('/home')
 end
