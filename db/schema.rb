132574d0f74f1fcd3acf1e96a7a9beca904b17de
Wayne Li <wli2@berkeley.edu>
2017-11-01 04:55:51 +0000

    Add more information to people

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171025092647) do
+ActiveRecord::Schema.define(version: 20171101045331) do
 
   create_table "families", force: :cascade do |t|
     t.string "family_name"
@@ -32,15 +32,19 @@ ActiveRecord::Schema.define(version: 20171025092647) do
     t.boolean "is_child"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "discovery"
+    t.string "hobbies"
+    t.string "skills"
+    t.string "activities"
+    t.string "committees"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email"
     t.string "password"
     t.string "full_name"
-    t.boolean "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
     t.string "salt"
     t.string "encrypted_password"
     t.string "password_digest"
