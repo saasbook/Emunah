e56855636125f6b7e9b0db1c6871e79918ec26cd
Wayne Li <wli2@berkeley.edu>
2017-11-01 04:57:38 +0000

    One more migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171101045331) do
+ActiveRecord::Schema.define(version: 20171101045656) do
 
   create_table "families", force: :cascade do |t|
     t.string "family_name"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20171101045331) do
     t.string "skills"
     t.string "activities"
     t.string "committees"
+    t.string "allow_contact"
   end
 
   create_table "users", force: :cascade do |t|
