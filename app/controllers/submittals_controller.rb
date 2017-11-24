0c022d599863f2e823cfe7c7c4058100aeb5227a
Casey <kazehero@github.com>
2017-11-24 02:44:26 -0500

    Fix update submittals

--- app/controllers/submittals_controller.rb
+++ app/controllers/submittals_controller.rb
@@ -16,6 +16,14 @@ class SubmittalsController < ApplicationController
     	@submittal = Submittal.find(params[:id])
 	end
 
+	def update 
+		@submittal = Submittal.find(params[:id])
+    	@submittal.update_attributes!(submittal_params)
+    	@family = Family.find_by(params[:family_id])
+    	flash[:notice] = "Submittal for #{@family.family_name} was successfully updated."
+    	redirect_to family_path(@family)
+	end 
+
 	def index
 		@user ||= User.find(session[:user_id]) if session[:user_id]
 		@family = Family.find_by(params[:family_id])
@@ -27,11 +35,12 @@ class SubmittalsController < ApplicationController
 		@family = Family.find(params[:family_id])
 		@family.submittals.build(submittal_params.merge(:family_name => @family.family_name)).save!
 		flash[:notice] = "Submittal successfully created for family: #{@family.family_name}"
-		# Tasks, need to change to handle multiple tasks later
+		# Tasks, need to change to handle multiple users later
+		# We need to also look for boolean too.
 		# currently we could do,
 		# :users => params[:task][:task_users] where params[:task][:task_users] is an array of User objects
 		# but params[:task][:task_users] front-end isn't there to handle User objects
-		@task = Task.create!(task_params.merge(:users => [User.first, User.second]))
+		@task = Task.create!(task_params.merge(:users => [User.first, User.second], :completed => false))
 		redirect_to family_path(@family)
 	end
 
