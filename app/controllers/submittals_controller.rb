5bdceb2c0330dec17d4486f2c78eed061b334cf5
Casey <kazehero@github.com>
2017-12-01 22:09:13 -0800

    fix

--- app/controllers/submittals_controller.rb
+++ app/controllers/submittals_controller.rb
@@ -44,10 +44,12 @@ class SubmittalsController < ApplicationController
 			# task_title, task_notes = task_params["title"], task_params["notes"]
 			# if !task_title.nil? and !task_notes.nil? and !task_title.empty? and !task_notes.empty?
 			# end
-
-            task_title, task_notes = params[:task][:title], params[:task][:notes]
-            if !task_title.nil? and !task_notes.nil? and !task_title.empty? and !task_notes.empty?
-			  @task = Task.create!(task_params.merge(:users => [User.first, User.second]))
+            
+            if !params[:task].nil?
+                task_title, task_notes = params[:task][:title], params[:task][:notes]
+                if !task_title.nil? and !task_notes.nil? and !task_title.empty? and !task_notes.empty?
+			      @task = Task.create!(task_params.merge(:users => [User.first, User.second]))
+                end
             end
 
             redirect_to family_path(@family), :flash => { :success => "Submittal successfully created for family: #{@family.family_name}"}
